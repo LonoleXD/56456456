a123c831eda77e6a2182d3c36f33593224915261
LonoleXD <sanzhar.aznakov@gmail.com>
2024-11-10 10:19:06 +0500

    Update main52121.py

--- main52121.py
+++ main52121.py
@@ -1,23 +1,23 @@
-
-def calculate(expression):
+def check_division(func):
     def hol(a, b):
-        return expression(a, b)
+        if b == 0:
+            return("На ноль делить нельзя")
+        return func(a, b)
     return hol
 
-@calculate
-def divide(a, b):
-    if(b != 0):
-        yield a + b
-        yield a - b
-        yield a * b
-        yield a / b
-    else:
-        yield a + b
-        yield a - b
-        yield a * b
-        yield "На ноль делить нельзя"
-print(divide(52, 2))
+@check_division
+def plus(a, b):
+    return a + b
+def minus(a, b):
+    return a - b
+def umn(a, b):
+    return a * b
+def delit(a, b):
+    return a / b
 
-
+print(plus(52, 2))
+print(minus(52, 2))
+print(umn(52, 2))
+print(delit(52, 2))
 
 
