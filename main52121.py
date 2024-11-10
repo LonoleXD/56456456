8a480e67ec133e3867e7cebe5cff89728274dd5b
LonoleXD <sanzhar.aznakov@gmail.com>
2024-11-10 10:00:17 +0500

    Update main52121.py

--- main52121.py
+++ main52121.py
@@ -1,33 +1,4 @@
-"""n = int(input())
-a = 1
-for i in range(a, n + 1):
-    print(i)"""
-from wheel.macosx_libfile import calculate_macosx_platform_tag
 
-"""class bober:
-    def __init__(self, f):
-        self.max = f
-        self.cnt = -2
-    def __iter__(self):
-        return self
-    def __next__(self):
-        self.cnt += 2
-        if(self.cnt > self.max):
-            raise StopIteration
-        else:
-            return self.cnt
-f = bober(51)
-for num in f:
-    print(num)"""
-
-"""def generator(f):
-    for i in range(1, f+1):
-        yield(i * i)
-
-f = int(input())
-counter = generator(f)
-for num in counter:
-    print(num)"""
 def calculate(expression):
     def hol(a, b):
         return expression(a, b)
